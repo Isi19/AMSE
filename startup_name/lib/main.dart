8e112d3cfa868177896beb3b697ef5cfd4ca18fb
isi19 <djamoineisidore@gmail.com>
2022-01-28 11:33:35 +0100

    Application with statefull widget

--- startup_name/lib/main.dart
+++ startup_name/lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';  // Add this line.
+import 'package:english_words/english_words.dart'; // Add this line.
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,12 +16,26 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          //child: Text('Hello World'),
-          child: Text(wordPair.asPascalCase),
+        body: Center(
+          // Drop the const, and
+          //child: Text('Hello World'),        // Replace this text...
+          child: RandomWords(), // With this text.
         ),
       ),
     );
   }
 }
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWorsState createState() => _RandomWorsState();
+}
+
+class _RandomWorsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
+    return Text(wordPair.asPascalCase);
+  }
 }
